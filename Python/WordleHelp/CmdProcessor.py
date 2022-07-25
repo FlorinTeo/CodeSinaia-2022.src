f878067350d0d7aee82d6731b41f4132cb319772
Florin Teodorescu <Florin.Teo@live.com>
2022-07-25 18:43:11 +0300

    Add WordleHelp cmd loop

---
+++ Python/WordleHelp/CmdProcessor.py
@@ -0,0 +1,19 @@
+class CmdProcessor:
+
+    def processHelp(self):
+        print("process command 'help'")
+
+    def processAdd(self, args = None):
+        print("Process command 'add'")
+
+    def processMatch(self, args = None):
+        print("Process command 'match'")
+    
+    def processReset(self, args = None):
+        print("Process command 'reset'")
+
+    def processStats(self, args = None):
+        print("Process command 'stats'")
+
+    def processConfig(self, args = None):
+        print("Process command 'config'")
